4ebe833e22b5981e8aa214cd84e10628c8d69f97
Rahul Gupta <rahul1990gupta@gmail.com>
2020-08-16 16:05:21 +0530

    release version 0.1

--- setup.py
+++ setup.py
@@ -5,8 +5,14 @@ if __name__ == "__main__":
     from setuptools import setup, find_packages
 
     setup(
-        name="indic_nlp_datasets", 
+        name="indic-nlp-datasets", 
         packages=find_packages(),
+        include_package_data=True,
+        install_requires=['tqdm'],
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+        ],
         entry_points={
             "console_scripts": [
                 "wikiextractor = idatasets.vendor.WikiExtractor:main"
